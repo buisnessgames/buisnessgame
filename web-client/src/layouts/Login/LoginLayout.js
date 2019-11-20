65adac53d0046ef64a438f8ea625d048fc4f723d
JaroslawPawliszcze <jpawliszcze@gmail.com>
2019-11-20 18:55:41 +0100

    Fix new element and readme

--- web-client/src/layouts/Login/LoginLayout.js
+++ web-client/src/layouts/Login/LoginLayout.js
@@ -2,21 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "shards-react";
 
 const LoginLayout = ({ children }) => (
-    <div class="login_background">
+    <>
+    <div className="login_background"></div>
   <Container fluid>
-    <Row>
-      <Col
-        className="main-content p-0"
-        lg={{ size: 10, offset: 2 }}
-        md={{ size: 9, offset: 3 }}
-        sm="12"
-        tag="main"
-      >
         {children}
-      </Col>
-    </Row>
   </Container>
-  </div>
+  </>
 );
 
 export default LoginLayout;
